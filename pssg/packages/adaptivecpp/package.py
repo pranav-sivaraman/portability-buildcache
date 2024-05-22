3a05861ee8b03a5d16dcd13f5b40e4fd00d5a594
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-05-22 18:49:30 -0400

    spack-pssg: cleanup adaptivecpp package

--- pssg/packages/adaptivecpp/package.py
+++ pssg/packages/adaptivecpp/package.py
@@ -17,50 +17,25 @@ class Adaptivecpp(CMakePackage):
     license("BSD-2-Clause")
 
     version("develop", branch="develop")
-    version(
-        "24.02.0",
-        sha256="180bdcbf40db9907ba5b3da06a57e779e1527c62528211f72e9d36a5e46b0956",
-    )
-    version(
-        "23.10.0",
-        sha256="9ac3567c048a848f4e6eadb15b09750357ae399896e802b2f1dcaecf8a090064",
-    )
-    version(
-        "0.9.4",
-        sha256="d9269c814f5e07b54a58bcef177950f222e22127c8399edc2e627d6b9e250763",
-    )
-    version(
-        "0.9.3",
-        sha256="6a2e2d81bd21209ad0726d5aa377321e177fecb775ad93078259835be0931f51",
-    )
-    version(
-        "0.9.2",
-        sha256="4b2308eb19b978a8528d55fe8c9fbb18d5be51aa0dd1a18a068946d8ddedebb1",
-    )
-    version(
-        "0.9.1",
-        sha256="f4adbe283d21272d5b5a1431650971c29360453c167b8c3f6ed32611322aa4b1",
-    )
-    version(
-        "0.9.0",
-        sha256="a7c565055fcb88dbca73693d3497fc8118e4f65b435e9bf136098a06b19dd8fc",
-    )
-    version(
-        "0.8.0",
-        sha256="18e1a14d2f5f86a9bb4b799818c086d017a7e397a5f58bb92463a90355951f44",
-    )
+    version("24.02.0", sha256="180bdcbf40db9907ba5b3da06a57e779e1527c62528211f72e9d36a5e46b0956")
+    version("23.10.0", sha256="9ac3567c048a848f4e6eadb15b09750357ae399896e802b2f1dcaecf8a090064")
+    version("0.9.4", sha256="d9269c814f5e07b54a58bcef177950f222e22127c8399edc2e627d6b9e250763")
+    version("0.9.3", sha256="6a2e2d81bd21209ad0726d5aa377321e177fecb775ad93078259835be0931f51")
+    version("0.9.2", sha256="4b2308eb19b978a8528d55fe8c9fbb18d5be51aa0dd1a18a068946d8ddedebb1")
+    version("0.9.1", sha256="f4adbe283d21272d5b5a1431650971c29360453c167b8c3f6ed32611322aa4b1")
+    version("0.9.0", sha256="a7c565055fcb88dbca73693d3497fc8118e4f65b435e9bf136098a06b19dd8fc")
+    version("0.8.0", sha256="18e1a14d2f5f86a9bb4b799818c086d017a7e397a5f58bb92463a90355951f44")
 
-    variant("cuda", default=False, description="Build with CUDA Backend")
-    variant("rocm", default=False, description="Build with ROCm Backend")
+    variant("cuda", default=False, description="Build with CUDA backend")
+    variant("rocm", default=False, description="Build with ROCm backend")
 
     depends_on("boost +filesystem +fiber +context cxxstd=17")
 
-    depends_on("libllvm@14:")
-    depends_on("llvm +clang")
+    depends_on("libllvm")
+    depends_on("llvm +clang", when="^llvm")
 
     depends_on("cuda", when="+cuda")
-
-    depends_on("hip", when="+rocm")
+    depends_on("rocm", when="+rocm")
 
     def cmake_args(self):
         spec = self.spec
@@ -72,7 +47,7 @@ class Adaptivecpp(CMakePackage):
             define("ACPP_VERSION_SUFFIX", "-"),
         ]
 
-        if "+cuda" in spec:
+        if spec.satisfies("+cuda"):
             args.extend(
                 [
                     from_variant("WITH_CUDA_BACKEND", "cuda"),
@@ -80,7 +55,7 @@ class Adaptivecpp(CMakePackage):
                 ]
             )
 
-        if "+rocm" in spec:
+        if spec.satisfies("+rocm"):
             args.extend(
                 [
                     from_variant("WITH_ROCM_BACKEND", "rocm"),
